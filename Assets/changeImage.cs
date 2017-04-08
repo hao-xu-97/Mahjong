62a4adb85456bbb7f6f16d6af02e8b18292f967f
Hao Xu <haoxunico@gmail.com>
2017-04-08 17:31:50 -0400

    create the first set of instructions

---
+++ Assets/changeImage.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class changeImage : MonoBehaviour {
+
+    private int counter;
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Image>().sprite = Resources.Load<Sprite>("instructions/s1");
+        counter = 2;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            string s = "instructions/s" + counter;
+            GetComponent<Image>().sprite = Resources.Load<Sprite>(s);
+            counter++;
+        }
+		
+	}
+}
