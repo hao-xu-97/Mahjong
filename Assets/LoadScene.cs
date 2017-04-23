fda67143e4438eaf19ccdd5b51082d27e357299f
Hao Xu <haoxunico@gmail.com>
2017-04-23 14:03:29 -0400

    added menu screen and setup for practice mode

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+    public void LoadTutorial()
+    {
+        SceneManager.LoadScene("tutorial");
+    }
+
+    public void LoadPractice()
+    {
+        SceneManager.LoadScene("practice");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
